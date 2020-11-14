59c5c6937fe36085c09d2988dcdfcb959fdcb798
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-11-14 08:39:38 +0530

    [Prajval]Employee Payroll Service

---
+++ EmployeeRepo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayrollADO.NET_Day26
+{
+    class EmployeeRepo
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollADO.NET_Day26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the payroll service");
+        }
+    }
+}
