180b05399800e40222e144b412083c04c7a12f02
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-11-14 09:12:43 +0530

    [Prajval]Established a connection with the database

--- EmployeeRepo.cs
+++ EmployeeRepo.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace EmployeePayrollADO.NET_Day26
 {
-    class EmployeeRepo
+    public class EmployeeRepo
     {
+        //UC1: Connecting to the already existing databse
+        public static string connectionString = @"Server=DESKTOP-C457C73\SQLEXPRESS; Database=payroll_service; User Id=prajval;Password=gonfreecs";
+        SqlConnection connection = new SqlConnection(connectionString);
     }
 }
